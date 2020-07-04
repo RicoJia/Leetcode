dd85addfbb10d830e8811362768e0b2c5cd36997
RicoJia <ruotongjia2020@u.northwestern.edu>
2020-07-04 16:13:05 -0500

    added 0067

--- template/test.cc
+++ template/test.cc
@@ -1,14 +1,9 @@
-#define CATCH_CONFIG_MAIN
-#include "../catch2.hpp"
 #include "solution.h"
-
 #include <iostream>
+
 using std::cout;
 using std::endl;
 
-TEST_CASE("test", "test_case")
-{
-	Solution s;
-	REQUIRE();
-
+int main(){
+    Solution s;
 }
