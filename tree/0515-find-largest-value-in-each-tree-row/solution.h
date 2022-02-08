cccc315667cefcd2c864f39617966bc490d6d7ae
rjia <rjia@accelrobotics.com>
2022-02-08 11:02:58 -0600

    new

--- tree/0515-find-largest-value-in-each-tree-row/solution.h
+++ tree/0515-find-largest-value-in-each-tree-row/solution.h
@@ -8,6 +8,17 @@ using std::vector;
 #include <algorithm>
 #include<climits>
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
 struct TreeNode {
     int val;
     TreeNode *left;
@@ -17,26 +28,60 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+
+// Method2 2022-02-08
 class Solution {
 public:
-    vector<int> largestValues(TreeNode* root) {
-        //Naive solution: 1. make an array of INT_MIN (you might need to dynamically allocate memory, which is slow.). 2. have a dfs. which passes in the last level, and increment.
-        // Compare with INT_MIN.
-        largest_vals_.reserve(10000);
-        dfs(root, 0);
-        return largest_vals_;
+    vector<int> vec_; 
+    TreeNode* build_tree(int l, int r){
+        if (l > r) return nullptr; 
+        int mid = (l+r)/2; 
+        TreeNode* root = new TreeNode(vec_.at(mid));
+        if (l == r) return root; 
+        root -> left = build_tree(l, mid - 1); 
+        root -> right = build_tree(mid+1, r); 
+        return root; 
     }
-    void dfs(TreeNode* root, int height){
-        if(!root) return;
-        if(largest_vals_.size() == height){
-            largest_vals_.push_back(root->val);
+    
+    TreeNode* sortedListToBST(ListNode* head) {
+        for (auto beg = head; beg != nullptr; beg = beg->next){
+            vec_.emplace_back(beg->val); 
         }
-        else{
-            largest_vals_.at(height) = std::max(largest_vals_.at(height), root->val);
+        return build_tree(0, vec_.size()-1); 
+    }
+};
+// Method-1 11-28-2020
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        // Naive solution: convert this list into an array, then convert this array into a binary search tree: find mid, the two mids.
+        // How to traverse a binary search tree: https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/solution/
+        // Better (more convoluted idea): 1. use a pointer, traverse thru the list, know the length. Then, dfs on the first half : 1. examine l <= r + 1, if so, make node and a ptr, and return
+        // the ptr. 2. find mid,  3. call dfs on left partition, connect the returned ptr to the left 4. call dfs on the right partition.
+        start_ = head;
+        auto trav = head;
+        int len = 0;
+        while (trav != nullptr){
+            ++len;
+            trav = trav -> next;
         }
-        dfs(root->left, height + 1);
-        dfs(root->right, height + 1);
+
+        return dfs(0, len - 1);
     }
 
-    vector<int> largest_vals_;
+private:
+    TreeNode* dfs(int l, int r){
+        if(l > r){      // why we prefer to return null -  because you don't have to deal with single leaves separately.
+            return nullptr;
+        }
+
+        int mid = (l + r)/2;
+        auto left = dfs(l, mid - 1);
+        auto root = new TreeNode(start_ -> val);
+        root -> left = left;
+        start_ = start_ -> next;
+        root -> right = dfs(mid + 1, r);
+        return root;
+    }
+    ListNode* start_;
 };
